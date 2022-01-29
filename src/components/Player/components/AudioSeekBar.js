f587133f39a0a3e8341dd7c06f8cc73423fedf70
Jagrutee Banda <jagruteebanda99@gmail.com>
2022-01-30 01:22:10 +0530

    handled sliding complete seekbar changes

--- src/components/Player/components/AudioSeekBar.js
+++ src/components/Player/components/AudioSeekBar.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {StyleSheet, View, Dimensions, Text} from 'react-native';
 import Slider from '@react-native-community/slider';
-import {useProgress} from 'react-native-track-player';
+import TrackPlayer, {useProgress} from 'react-native-track-player';
+import {useEffect} from 'react/cjs/react.development';
 
 const {width, height} = Dimensions.get('window');
 
 const AudioSeekBar = props => {
-  const {
-    audioInfo = {},
-    seekValue = 0.0,
-    setSeekValue,
-    audioDuration = 0.0,
-  } = props;
+  const {audioInfo = {}, seekValue, setSeekValue} = props;
 
   const progress = useProgress();
 
@@ -25,19 +21,19 @@ const AudioSeekBar = props => {
     return `${mins}:${secs}`;
   };
 
-  const formatAudioDuration = time => {
-    return `${time.toFixed(2)}`;
+  const handleSeekBarChange = async value => {
+    await TrackPlayer.seekTo(value);
   };
 
   return (
     <View style={styles.seekBarView}>
       <Slider
         style={{width: width - 8, height: 25}}
-        step={0.01}
-        onValueChange={value => setSeekValue(value)}
-        value={progress.position}
+        step={1}
+        onSlidingComplete={v => handleSeekBarChange(v)}
+        value={seekValue}
         minimumValue={0}
-        maximumValue={audioInfo.duration}
+        maximumValue={progress.duration}
         thumbTintColor={'#cc0066'}
         minimumTrackTintColor={'#cc0066'}
         maximumTrackTintColor={'#e6e6e6'}
@@ -50,7 +46,9 @@ const AudioSeekBar = props => {
           justifyContent: 'space-between',
           height: 25,
         }}>
-        <Text style={{color: '#cc0066'}}>{secsToTimestamp(Math.floor(progress.position))}</Text>
+        <Text style={{color: '#cc0066'}}>
+          {secsToTimestamp(Math.floor(progress.position))}
+        </Text>
         <Text style={{color: '#e6e6e6'}}>
           {secsToTimestamp(audioInfo.duration)}
         </Text>
